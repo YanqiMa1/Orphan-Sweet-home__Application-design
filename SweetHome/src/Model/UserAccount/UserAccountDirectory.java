013a4578ad2bc52d73d60df103ab43500ec011c4
ZihaoLiuNU <liu.zihao@northeastern.edu>
2023-04-14 17:44:39 -0400

    Update UserAccountDirectory.java

--- SweetHome/src/Model/UserAccount/UserAccountDirectory.java
+++ SweetHome/src/Model/UserAccount/UserAccountDirectory.java
@@ -4,6 +4,9 @@
  */
 package Model.UserAccount;
 
+import Model.EcoSystem.Network;
+import Model.Enterprise.Enterprise;
+import Model.Organization.Organization;
 import java.util.ArrayList;
 
 /**
@@ -21,62 +24,34 @@ public class UserAccountDirectory {
     public ArrayList<UserAccount> getUserAccountList() {
         return userAccountList;
     }
-     
-    public UserAccount createUserAccount(String username, String password, Role role) {
-        UserAccount user = new UserAccount(username, password, role);
-        
-        userAccountList.add(user);
-        return user;
-    }
-    
-    public UserAccount findById(String id) {
-        for(UserAccount u: this.userAccountList) {
-            if(u.getAccountId().equals(id)) {
-                return u;
-            }
-        }
-        
-        return null;
-    }
-    
-    public UserAccount getUserAccount(String username, String password, Role role) {
-        for(UserAccount u: this.userAccountList) {
-            if(u.getUsername().equals(username) && u.getPassword().equals(password) && u.getRole().equals(role)) {
-                return u;
-            }
-        }
-        
-        return null;
-    }
-    
-    public Boolean accountExists(String username) {
-        for(UserAccount u: this.userAccountList) {
-            if(u.getUsername().equals(username)) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    
-    public UserAccount authenticateUser(String name, String password) {
-        for(UserAccount ua: this.userAccountList) {
-            if(ua.getUsername().equals(name) && ua.getPassword().equals(password)) {
+
+    public UserAccount authenticateUser(String username, String password) {
+        for (UserAccount ua : userAccountList) {
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)) {
                 return ua;
             }
         }
         return null;
     }
-    
-    
-      public UserAccount findByUsername(String username) {
-        for(UserAccount ua: this.userAccountList) {
-            if(ua.getUsername().equals(username)) {
-                return ua;
+
+    public UserAccount createUserAccount(String username, String password, Role role, Network network, Enterprise enterprise, Organization organization) {
+        UserAccount userAccount = new UserAccount();
+        userAccount.setUsername(username);
+        userAccount.setPassword(password);
+        userAccount.setRole(role);  
+        userAccount.setNetwork(network);
+        userAccount.setEnterprise(enterprise);
+        userAccount.setOrgainization(organization);
+        userAccountList.add(userAccount);
+        return userAccount;
+    }
+
+    public boolean userNameIsUnique(String username) {
+        for (UserAccount ua : userAccountList) {
+            if (username.equalsIgnoreCase(ua.getUsername())) {
+                return false;
             }
         }
-        
-        return null;
+        return true;
     }
 }
